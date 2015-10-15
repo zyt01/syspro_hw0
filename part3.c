c6a488a46c844c8f1e803a5a01e58c761b8e0301
张宇彤 <yutong4391@163.com>
2015-10-15 17:04:01 +0800

    Create part3.c

---
+++ part3.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Concatenates the strings "Hello " and "World!" together, and
+ * prints the concatenated string.
+ */
+void eleven()
+{
+	char s[] = "Hello ";
+	strcat(s, "World!");
+	printf("%s\n", s);
+}
+
+
+/**
+ * Creates an array of values containing the values {0.0, 0.1, ..., 0.9}.
+ */
+void twelve()
+{
+	float *values = (float*)malloc(sizeof(float)*11);
+
+	int i, n = 10;
+	for (i = 0; i < n; i++)
+		values[i] = (float)i / n;
+
+	for (i = 0; i < n; i++)
+		printf("%f ", values[i]);
+	printf("\n");
+
+	free(values);
+}
+
+
+/**
+ * Creates a 2D array of values and prints out the values on the diagonal.
+ */
+void thirteen()
+{
+	int **values;
+
+	int i, j;
+	values = (int **)malloc(10 * sizeof(int *));
+	for (i = 0; i < 10; i++)
+		values[i] = (int *)malloc(10 * sizeof(int *));
+	for (i = 0; i < 10; i++)
+		for (j = 0; j < 10; j++)
+			values[i][j] = i * j;
+
+	for (i = 0; i < 10; i++)
+		printf("%d ", values[i][i]);
+	printf("\n");
+}
+
+
+/**
+ * Prints out a specific string based on the input parameter (s).
+ *
+ * @param s
+ *     Input parameter, used to determine which string is printed.
+ */
+void fourteen(const char *s)
+{
+	if(!strcmp(s,"white"))
+		printf("Green and WHITE!\n");
+
+	else if(!strcmp(s,"green"))
+		printf("GREEN and white!\n");
+	
+	else
+		printf("green and white!\n");
+}
+
+
+/**
+ * Prints out a specific string based on the input parameter (value).
+ *
+ * @param value
+ *     Input parameter, used to determine which string is printed.
+ */
+void fifteen(const int value)
+{
+	switch (value)
+	{
+		case 1:
+			printf("You passed in the value of one!\n");
+			break;
+
+		case 2:
+			printf("You passed in the value of two!\n");
+			break;
+
+		default:
+			printf("You passed in some other value!\n");
+			break;
+	}
+}
