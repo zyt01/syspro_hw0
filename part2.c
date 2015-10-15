b42596e7ee1cef4c6bff708bdd4367fd2ee95b56
张宇彤 <yutong4391@163.com>
2015-10-15 16:49:41 +0800

    Create part2.c

---
+++ part2.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Returns a new pointer (float *p) which contains the value of the
+ * input pointer (int *x).
+ *
+ * @param x
+ *     Input parameter, whose value will be returned as a (float *).
+ *
+ * @returns
+ *     A new pointer, allocated on the heap and not freed, that
+ *     contains the value of the input parameter.
+ */
+float * six(const int *x)
+{
+	float t = *x;
+	float *p = &t;
+	return p;
+}
+
+
+/**
+ * Takes two inputs, a and b such that (a < b), and makes a and b equal
+ * via a while-loop.  Prints out their value before returning.
+ * 
+ * @param a
+ *     Input parameter a, where a < b.
+ *
+ * @param b
+ *     Input parameter b, where b > a.
+ */
+void seven(int a, const int b)
+{
+	while (a != b)
+	{
+		a++;
+	}
+
+	printf("%d is now equal to %d\n", a, b);
+}
+
+
+/**
+ * Constructs a C-string, character by character, and prints out the full
+ * string "Hello".
+ */
+void eight()
+{
+	char *s = (char *)malloc(5 * sizeof(char));
+
+	s[0] = 'H';
+	s[1] = 'e';
+	s[2] = 'l';
+	s[3] = 'l';
+	s[4] = 'o';
+	s[5] = '\0';
+	printf("%s\n", s);
+
+	free(s);
+}
+
+
+/**
+ * Assigns a pointer (float *p) a numeric value (12.5).
+ */
+void nine()
+{
+	float *p;
+	float t = 12.5;
+	p = &t;
+
+	printf("The value of p is: %f\n", *p);
+}
+
+
+/**
+ * Reset the value of x to zero.
+ *
+ * @param x
+ *     Pointer to reset to 0.
+ */
+void ten(int *x)
+{
+	*x = 0;
+}
